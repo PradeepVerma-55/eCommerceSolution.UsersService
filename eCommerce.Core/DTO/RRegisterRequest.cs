a72de5b26fe43c2a9c0929a0a203e9a8cf550f4d
Pradeep Verma <pradeepnetcore@outlook.com>
2025-11-19 21:19:55 +0530

    Adding Entities and DTO's

---
+++ eCommerce.Core/DTO/RRegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace eCommerce.Core.DTO
+{
+    public record RRegisterRequest(
+        string? Email,
+        string? Password,
+        string? PersonName,
+        GenderOptions Gender
+        );
+}
---
+++ eCommerce.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿namespace eCommerce.Core.Entities
+{
+    /// <summary>
+    /// define the Application user class which acts as a data model for user information that will be stored in the system.
+    /// </summary>
+    public class ApplicationUser
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string? Password { get; set; }
+        public  string? PersonName { get; set; }
+        public string? Gender { get; set; }
+
+
+    }
+}
