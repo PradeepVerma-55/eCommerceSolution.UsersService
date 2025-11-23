e7140dcdb6c3fe602aeb667716146e3ed991e747
Pradeep Verma <pradeepnetcore@outlook.com>
2025-11-23 17:06:12 +0530

    Added RegisterRequestValidator to Validate Register Request

---
+++ eCommerce.Core/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,32 @@
+﻿using eCommerce.Core.DTO;
+using FluentValidation;
+
+namespace eCommerce.Core.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+
+            RuleFor(model => model.Email)
+
+               .NotEmpty().WithMessage("email must not be empty")
+
+               .EmailAddress().WithMessage("email must be valid");
+
+            RuleFor(model => model.Password)
+                .NotEmpty().WithMessage("password must be not empty")
+                .MinimumLength(6).WithMessage("password must be atleas 6 chars")
+                .MaximumLength(20).WithMessage("password max length is 20");
+
+            RuleFor(model => model.PersonName)
+                .NotEmpty()
+                .WithMessage("name must be not empty");
+
+            RuleFor(model => model.Gender)
+                .IsInEnum()
+                .WithMessage("must be valid value");
+
+        }
+    }
+}
